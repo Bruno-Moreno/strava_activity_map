b0e5644af37eb703dac9ce7cdcbe2d0925b3744c
Nelson Bruno Moreno <55368022+Bruno-Moreno@users.noreply.github.com>
2024-08-09 11:03:06 -0400

    strava and map update

--- src/create_map.py
+++ src/create_map.py
@@ -22,8 +22,10 @@ def iterate_over_routes(data_path):
     df = pd.DataFrame() 
     for d in data_files:
         df_ = pd.read_csv(f'{data_path}//{d}')
+        df_["date"] = d.split("__")[1].split(".")[0]
+        df_["date"] = pd.to_datetime(df_["date"]).dt.date
         df = pd.concat([df, df_], axis = 0)
-
+    
     return df 
 
 
@@ -36,8 +38,10 @@ if __name__ == "__main__":
         lat='lat', 
         lon='long',
         hover_name='activity_name',  # Assuming you have a column with run IDs
-        zoom=10,
-        color = 'activity_type'
+        hover_data = ['date'],
+        zoom=8.5,
+        color = 'activity_type',
+        center={"lat": -33.55, "lon": -70.60},
     )
 
     fig.update_layout(mapbox_style="open-street-map")
