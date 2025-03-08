95e38124c04f26ee88c0338669c2ebfa60eb1763
Nelson Bruno Moreno <55368022+Bruno-Moreno@users.noreply.github.com>
2025-03-07 23:22:29 -0300

    format: ruff

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ from dotenv import load_dotenv
 from src.strava_connection import strava_connection
 from src.download_strava_activities import download_strava_activities
 from src.read_activities_data import get_activities_data
-from src.dash_app import StravaActivitiesApp  
+from src.dash_app import StravaActivitiesApp
 
-data_path = "data" 
+data_path = "data"
 favicon = "favicon.ico"
 
-def launch(new_activities):
 
+def launch(new_activities):
     if new_activities == "y":
         print("Reading New Activities")
         client = strava_connection()
@@ -17,12 +17,11 @@ def launch(new_activities):
 
     df = get_activities_data(data_path)
     strava_app = StravaActivitiesApp(df, favicon)
-    strava_app.create_app() 
+    strava_app.create_app()
     strava_app.run()
 
 
-if __name__ == '__main__':
-    
-    load_dotenv() 
+if __name__ == "__main__":
+    load_dotenv()
     new_activities = input("Read new activities? (y/n): ")
     launch(new_activities)
--- src/download_strava_activities.py
+++ src/download_strava_activities.py
@@ -1,41 +1,47 @@
-import pandas as pd 
-import os 
+import pandas as pd
+import os
+
 
 def get_already_read_files(data_path):
+    already_read_files = [
+        f
+        for f in os.listdir(data_path)
+        if os.path.isfile(os.path.join(data_path, f)) and f.endswith("csv")
+    ]
 
-    already_read_files = [f for f in os.listdir(data_path) if os.path.isfile(os.path.join(data_path, f)) and f.endswith('csv')]
+    return already_read_files
 
-    return already_read_files 
 
 def download_strava_activities(client, data_path):
-
     already_read_files = get_already_read_files(data_path)
     activities = client.get_activities(limit=100)
 
-    for a in activities: 
+    for a in activities:
         id = a.id
         activity_name = a.name
         activity_type = a.type.root
         start_date_date = a.start_date
         start_date_str = start_date_date.strftime("%Y%m%d")
-        distance = f"{float(a.distance)/1000:.2f}K"
+        distance = f"{float(a.distance) / 1000:.2f}K"
 
-        csv_name = f"{activity_name}__{start_date_str}.csv" 
+        csv_name = f"{activity_name}__{start_date_str}.csv"
         print(f"Activity: {activity_name}, Type: {activity_type}")
 
-        if (activity_type not in ["WeightTraining", "Workout", "VirtualRide", "Treadmill"]) and (csv_name not in already_read_files): 
-            
-            activity = client.get_activity_streams(id, types = ["latlng", "altitude"])
+        if (
+            activity_type
+            not in ["WeightTraining", "Workout", "VirtualRide", "Treadmill"]
+        ) and (csv_name not in already_read_files):
+            activity = client.get_activity_streams(id, types=["latlng", "altitude"])
 
             try:
                 latlng = activity["latlng"].data
                 altitude = activity["altitude"].data
-                data_ = pd.DataFrame([*latlng], columns=['lat','long'])
-                data_['altitude'] = altitude
+                data_ = pd.DataFrame([*latlng], columns=["lat", "long"])
+                data_["altitude"] = altitude
                 data_["distance"] = distance
-                data_['activity_id'] = id 
-                data_['activity_name'] = activity_name        
-                data_['activity_type'] = activity_type    
-                data_.to_csv(f'{data_path}//{csv_name}', index = False)
+                data_["activity_id"] = id
+                data_["activity_name"] = activity_name
+                data_["activity_type"] = activity_type
+                data_.to_csv(f"{data_path}//{csv_name}", index=False)
             except KeyError:
                 print("Activity doesn't have gps data")
