28fd1825dafe38b18147aacd4bec91575d550279
Nelson Bruno Moreno <55368022+Bruno-Moreno@users.noreply.github.com>
2025-03-07 23:22:29 -0300

    format: ruff

--- src/strava_connection.py
+++ src/strava_connection.py
@@ -1,28 +1,30 @@
 from stravalib import Client
-import os 
+import os
+
 
 def get_strava_authorization_code():
-
     client = Client()
     url = client.authorization_url(
-        client_id=os.environ["STRAVA_CLIENT_ID"] ,
+        client_id=os.environ["STRAVA_CLIENT_ID"],
         redirect_uri="http://127.0.0.1:5000/authorization",
-    ) 
+    )
     print("Enter to the link below and copy the strava code \n")
     print(url)
     print("\n")
 
-def get_strava_token(code):
 
+def get_strava_token(code):
     client = Client()
     token_response = client.exchange_code_for_token(
-        client_id=os.environ["STRAVA_CLIENT_ID"], client_secret=os.environ["STRAVA_CLIENT_SECRET"], code=code
+        client_id=os.environ["STRAVA_CLIENT_ID"],
+        client_secret=os.environ["STRAVA_CLIENT_SECRET"],
+        code=code,
     )
 
     return token_response
 
-def strava_login(token_response):
 
+def strava_login(token_response):
     client = Client(
         access_token=token_response["access_token"],
         refresh_token=token_response["refresh_token"],
@@ -32,15 +34,13 @@ def strava_login(token_response):
     athlete = client.get_athlete()
     print(f"Hi, {athlete.firstname} Welcome to stravalib!")
 
-    return client 
+    return client
 
-def strava_connection(): 
 
+def strava_connection():
     get_strava_authorization_code()
     code = input("Insert Autorization Code: ")
     token_response = get_strava_token(code)
     client = strava_login(token_response)
 
     return client
-
-
